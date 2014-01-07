4702aa343a4adfe757e1520634efa11f6fbe0726
Matthias Eiserloh <matthias_eiserloh@web.de>
2014-01-07 19:44:25 +0100

    Changed Designer's name

--- config.py
+++ config.py
@@ -156,7 +156,7 @@ POP_UP_ITEM_TEXT =  codecs.decode("Bitte Beschreibung eingeben und bestaetigen",
 POP_UP_SAVE_TEXT = codecs.decode("Bitte Dateinamen für das zu speichernde Bild eingeben", "utf-8")
 
 CREDITS_HEADER =  [codecs.decode(word,"utf-8") for word in ["PROGRAMMING","DESIGN","SPECIAL THANKS"]]
-CREDITS_NAMES =  [codecs.decode(word,"utf-8") for word in ["Matthias Eiserloh", "Markus Wolf","Vincent van Gogh", "to all testers for their effort and input","to vincent for his amazing artwork"]]
+CREDITS_NAMES =  [codecs.decode(word,"utf-8") for word in ["Matthias Eiserloh", "Markus Wolf","Vincent Grahn", "to all testers for their effort and input","to vincent for his amazing artwork"]]
 
 
 
